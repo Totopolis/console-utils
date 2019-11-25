5a64a33542eae9f15f70260eeb9d4927191318f4
Konstantin Anikeev <anikeew@gmail.com>
2019-11-25 20:01:56 +0300

    Use github actions for deploy and publish
    deploy to myget
    
    Add nuget cfg
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip
    
    wip1
    
    wip2

---
+++ src/ConsoleHelpers/BashUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleHelpers
+{
+    public static class BashUtils
+    {
+        public static string Bash(this string cmd)
+        // TODO: params - showOutput = false, enableExceptions = false
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            Console.WriteLine(cmd);
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(result);
+
+            return result;
+        }
+    }
+}
