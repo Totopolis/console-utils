976f7a504896968cc4e41281cac5921d8750d7e5
Konstantin Anikeev <anikeew@gmail.com>
2019-11-25 20:01:56 +0300

    first commit

---
+++ src/ConsoleHelpers/OutputUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleHelpers
+{
+    public static class OutputUtils
+    {
+        // TODO: toLog copy stream
+        public static void Out(this string message, bool underLine = false)
+        {
+            Console.WriteLine(message);
+            if (underLine)
+                Console.WriteLine(string.Empty.PadRight(message.Length, '='));
+        }
+    }
+}
